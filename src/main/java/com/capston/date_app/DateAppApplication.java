3df524f924d23c3ec91e2468680f7d3fd8faafe4
seongyong0908 <seongyong0908@gmail.com>
2026-03-27 17:29:34 +0900

    feat: 프로젝트 초기 세팅 완료

---
+++ src/main/java/com/capston/date_app/DateAppApplication.java
@@ -0,0 +1,13 @@
+package com.capston.date_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capston/date_app/DateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capston.date_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
