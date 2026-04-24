6c5d519c2e34f80b103a1d95f270580bfaa6d473
dugowbf <wogud145@gmail.com>
2026-04-24 21:27:20 +0900

    혼자서 서버 부팅이 되는지 확인하기 위해 테스트 겸 기존 코드는 냅두고 (주석처리함) 추가하여 실행해봄 -류재형

--- src/main/java/com/capston/date_app/Place.java
+++ src/main/java/com/capston/date_app/Place.java
@@ -1,3 +1,5 @@
+/*
+
 package com.capston.date_app; // 패키지 경로 확인!
 
 import jakarta.persistence.Entity;
@@ -22,3 +24,33 @@ public class Place {
     private Double latitude;   // 위도
     private Double longitude;  // 경도
 }
+ */
+
+package com.capston.date_app;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "place")
+@Getter
+@Setter
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long course_id;
+    private String place_name;
+    private Double latitude;
+    private Double longitude;
+    private Integer visit_order;
+    private String description;
+}
+
